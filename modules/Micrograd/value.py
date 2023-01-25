c86d49ddc585541c7664c6bc9d81f9561bab5f20
arigato <arigato@arigato.com>
2023-01-25 11:18:19 +0800

    Finished training a model

--- modules/Micrograd/value.py
+++ modules/Micrograd/value.py
@@ -210,4 +210,4 @@ class Value:
         return other * self**-1
     
     def __repr__(self):
-        return f'Value(data={self.data}, data={self.grad})'
+        return f'Value(data={self.data}, grad={self.grad})'
